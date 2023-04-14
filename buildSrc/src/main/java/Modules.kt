1f1ad04b8b73b8e9ff5d7e4adec17013aee71ffc
hosval29 <hosval29@gmail.com>
2023-04-14 10:05:56 -0500

    [ADD] Setup

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,17 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onboardingDomain = ":onboarding:onboarding_domain"
+    const val onboardingPresentation = ":onboarding:onboarding_presentation"
+
+    const val moviesData = ":movies:movies_data"
+    const val moviesDomain = ":movies:movies_domain"
+    const val moviesPresentation = ":movies:movies_presentation"
+
+    const val seriesData = ":series:series_data"
+    const val seriesDomain = ":series:series_domain"
+    const val seriesPresentation = ":series:series_presentation"
+}
